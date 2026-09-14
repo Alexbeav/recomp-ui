8871ac5f47dc940114752426d7e5c757197bdec7
Alexandros Mandravillis <Alexbeav@live.com>
2026-09-14 13:51:01 +0300

    launcher: list .chd in the PSX filter label

--- src/consoles/psx/psx_profile.h
+++ src/consoles/psx/psx_profile.h
@@ -127,7 +127,7 @@ static const SystemProfile kSystemProfilePsx = {
     /* screen_kind_names */ NULL,   /* legacy Raw/CRT/Composite/Trinitron set */
     /* screen_kind_count */ 0,
     /* rom_filter        */ { kPsxDiscPatterns, LNG_PSX_DISC_PATTERN_COUNT,
-                              "PlayStation disc / SBI (.cue/.bin/.car/.sbi)" },
+                              "PlayStation disc / SBI (.cue/.bin/.car/.sbi/.chd)" },
     /* renderer_labels   */ NULL,
     /* hide_audio_freq   */ 0,
     /* brand             */ "brand_psx.tga",
